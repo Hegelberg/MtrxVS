018e03b3f9d925f3e21a0d8d7654a751baae8c64
Hegelberg <terrazas.jorden9@gmail.com>
2018-01-16 17:28:52 +0000

    1/16/18

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
+// This is a test comment for sublime text 2
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
-
+  win = new BrowserWindow({width: 650, height: 650})
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
